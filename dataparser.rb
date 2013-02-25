1028e32a1205264c2a8e91f7055697255ca127e3
Victoria Friedman <victoria.m.friedman@gmail.com>
2013-02-24 19:05:11 -0500

    creating people in comma.txt

--- dataparser.rb
+++ dataparser.rb
@@ -1,8 +1,53 @@
-# Write a Ruby program to first assemble a single set of records by parsing data from 3 different file formats and then display these records sorted in 3 different ways.
+# Write a Ruby program to first assemble a single set of records by parsing data from 
+#3 different file formats and then display these records sorted in 3 different ways.
+
+
+
+#Here's a general idea for you to start with
+
+def open_file (record)
+  File.open(record)
+end
+
+def split (split_param, item)
+  item.split(split_param)
+end
+
+class Person
+  attr_accessor :last_name, :first_name, :gender, :color, :date_of_birth
+
+  def initialize(params={})
+    @last_name = params[:last_name]
+    @first_name = params[:first_name]
+    @gender = params[:gender]
+    @color = params[:color]
+    @date_of_birth = params[:date_of_birth]
+    @middle_initial = params[:middle_initial]
+  end
+
+end
+
+open_file('comma.txt').each_with_index do |line, index|
+  people = []
+  people << Person.new(:last_name => split(', ', line)[0], 
+                       :first_name => split(', ', line)[1])
+  people[0]
+end
+
+# split_param = ', '
+# split_param = ' | '
+# split_param = ' '
+
+
+
+
+
 
 # INPUT DATA
 
-# A record consists of the following 5 fields: last name, first name, gender, date of birth and favorite color. You will be given 3 files, each containing records stored in a different format.
+# A record consists of the following 5 fields: last name, first name, gender, 
+#date of birth and favorite color. You will be given 3 files, each containing records 
+#stored in a different format.
 
 # The pipe-delimited file lists each record as follows:
 # LastName | FirstName | MiddleInitial | Gender | FavoriteColor | DateOfBirth
@@ -13,7 +58,9 @@
 # The space-delimited file looks like this:
 # LastName FirstName MiddleInitial Gender DateOfBirth FavoriteColor
 
-# You may assume that the delimiters (commas, pipes and spaces) do not appear anywhere in the data values themselves. Write a Ruby program to read in records from these files and combine them into a single set of records.
+# You may assume that the delimiters (commas, pipes and spaces) do not appear 
+#anywhere in the data values themselves. Write a Ruby program to read in records 
+#from these files and combine them into a single set of records.
 
 
 # DISPLAY REQUIREMENTS
@@ -21,5 +68,6 @@
 # Output 1 – sorted by gender (females before males) then by last name ascending.
 # Output 2 – sorted by birth date, ascending.
 # Output 3 – sorted by last name, descending.
-# Ensure that fields are displayed in the following order: last name, first name, gender, date of birth, favorite color.
+# Ensure that fields are displayed in the following order: last name, first name, 
+  #gender, date of birth, favorite color.
 # Display dates in the format MM/DD/YYYY.
